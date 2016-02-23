0f29411f5868e0b2fb0bf2e7dedeae1bf11e2fb8
Ionut Firatoiu <firatoiu.ionut@gmail.com>
2016-02-23 15:34:37 +0200

    componenta board, componenta point(still trying)

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,38 +3,54 @@ require('styles/App.css');
 
 import React from 'react';
 
-/*var Board = React.createClass ({
-  getInitialState: function() {
-    var board = [[-3,-3,-3],[-3,-3,-3],[-3,-3,-3]];
-    return {
-      board: board
+class Point extends React.Component {
+  position(x, y) {
+    /*let x;
+    let y;*/
+    for (let i = 0; i < 24; i++){
+      if (i===0||i===1||i===2){
+        y=0;
+      }
     }
+},
+
+  render() {
+    var style = this.getAbsolutePosition(this.props.index);
+    return (<div style={style}>{this.props.index}</div>)
   }
-});*/
+}
+
+class Border extends React.Component {
+  render() {
+    return (
+      <div id="square">
+        <div id="small-square"></div>
+        <div id="small-square"></div>
+        <div id="small-square"></div>
+        <div id="small-square"></div>
+        <div id="second-square">
+          <div id="second-small-square"></div>
+          <div id="second-small-square"></div>
+          <div id="second-small-square"></div>
+          <div id="second-small-square"></div>
+          <div id="third-square"></div>
+        </div>
+      </div>
+    )
+  }
+}
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <div id="square">
-          <div id="small-square"></div>
-          <div id="small-square"></div>
-          <div id="small-square"></div>
-          <div id="small-square"></div>
-          <div id="second-square">
-            <div id="second-small-square"></div>
-            <div id="second-small-square"></div>
-            <div id="second-small-square"></div>
-            <div id="second-small-square"></div>
-            <div id="third-square"></div>
-          </div>
-        </div>
-       </div>
+        {points}
+        <Border/>
+      </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
